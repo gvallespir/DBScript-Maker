5b9b1911a53e95d2dd1ddbe81b1e1f941a35bb36
Guillermo Vallespir <guille@iMac-de-Guillermo.local>
2018-10-10 23:52:09 -0300

    Primer commit

---
+++ src/com/guillermovallespir/dbscriptmaker/DBScriptMaker.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guillermovallespir.dbscriptmaker;
+
+import com.guillermovallespir.dbscriptmaker.Visual.Panel;
+
+/**
+ *
+ * @author guille
+ */
+public class DBScriptMaker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Panel panel = new Panel();
+        panel.setVisible(true);
+    }
+    
+}
